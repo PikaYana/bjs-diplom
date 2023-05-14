bbd52c0ac4af6d22d3fb25e6b457d36860bfc3b0
Pika Yana <pikayana611@gmail.com>
2023-05-15 02:16:31 +0300

    внесены поправки 14.05)

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,29 +1,25 @@
 'use strict';
 
-class UserForm {
-  constructor() {
-    this.loginFormCallback = (data) => {
-      ApiConnector.login(data, (response) => {
-        if (response.success) {
-         console.log(response);
-          location.reload();
-        } else {
-            this.setLoginErrorMessage(response.error);
-        }
-      });
-    };
-    this.registerFormCallback = (data) => {
-        ApiConnector.register(data,(response) => {
-            if (response.success) {
-                console.log(response);
-                location.reload();  
-            } else {
-              this.setRegisterErrorMessage(response.error);
-            }
-        });
-    }
-  }
-}
+const { response } = require("express");
 
 const userForm = new UserForm();
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login(data, (response) => {
+        if (response.success) {
+          location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error);
+        }
+      });
+}
+
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register(data,(response) => {
+        if (response.success) {
+            location.reload();  
+        } else {
+          userForm.setRegisterErrorMessage(response.error);
+        }
+    });
+}
 
